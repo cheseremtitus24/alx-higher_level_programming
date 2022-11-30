e0d57084b848d09d91a48bc0f4aa886fdf6120b7
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-11-30 11:56:34 +0300

    new file:   12-fizzbuzz.py 	new file:   12-main.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100 + 1):
+        if (i == 100):
+            if i % 5 == 0:
+                print("{}".format("Buzz"), end="")
+            else:
+                print("{}".format(i), end="")
+
+            continue
+        else:
+            if i % 5 == 0 and i % 3 == 0:
+                print("{}".format("FizzBuzz"), end=" ")
+
+            elif i % 5 == 0:
+                print("{}".format("Buzz"), end=" ")
+            elif i % 3 == 0:
+                print("{}".format("Fizz"), end=" ")
+            else:
+                print("{}".format(i), end=" ")
+
+
+fizzbuzz()
