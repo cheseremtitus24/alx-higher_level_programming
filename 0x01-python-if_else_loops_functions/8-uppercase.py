2aaf838b63a1f9433d66cd3fa6ecbb5a5fa8f48b
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-11-30 11:21:12 +0300

    Feature: Converts Lowercase to Upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    res_string = list()
+
+    for letter in str:
+        try:
+            char_index = lowercase.index(letter)
+            res_string.append(uppercase[char_index])
+        except BaseException:
+            res_string.append(letter)
+
+    print("".join(res_string))
