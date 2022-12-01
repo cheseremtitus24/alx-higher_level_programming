86a3c6c6f0770001d14056ad82004d170a48c18f
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-01 15:46:50 +0300

    modified:   2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     if (argc == 1):
         print("{} arguments.".format(argc - 1))
     else:
-        print("{} arguments:".format(argc - 1))
-    for i in range(argc):
-        if (argc == 1):
-            continue
-        print("{}: {}".format(i, sys.argv[i]))
+        if argc == 2:
+            print("{} argument:".format(argc - 1))
+        else:
+            print("{} arguments:".format(argc - 1))
+        for i in range(argc):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, sys.argv[i]))
