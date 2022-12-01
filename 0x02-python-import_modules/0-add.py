8d08c55af72aa401429cd56d18280586a31d34be
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-01 14:36:27 +0300

    modified:   0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)#!/usr/bin/python3
-
+from add_0 import add
 if __name__ == '__main__':
     a = 1
     b = 2
