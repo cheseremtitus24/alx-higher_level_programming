1a5ff372d75352c557d20c93d4e19bb0038bbbfe
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-07 16:23:04 +0300

    modified:   3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     size = len(my_list)
     if size == 0:
         return my_list
-    for i in range(-1, (-size - 1), -1):
+    my_list.reverse()
+    for i in range(size):
         print("{:d}".format(my_list[i]))
