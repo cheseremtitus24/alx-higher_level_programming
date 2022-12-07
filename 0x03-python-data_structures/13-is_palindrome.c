63642f8aa584a2bf2c22f898e6f08d94af9c94bf
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-07 23:49:24 +0300

    modified:   13-is_palindrome.c 	modified:   lists.h

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,107 +1,115 @@
-// C program for array implementation of stack
 #include "lists.h"
- 
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
-struct Stack* createStack(unsigned capacity)
+
+/* function to create a stack of given capacity. It initializes size of*/
+struct Stack *
+createStack (unsigned capacity)
 {
-    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (int*)malloc(stack->capacity * sizeof(int));
-    return stack;
+  struct Stack *stack = (struct Stack *) malloc (sizeof (struct Stack));
+  stack->capacity = capacity;
+  stack->top = -1;
+  stack->array = (int *) malloc (stack->capacity * sizeof (int));
+  return (stack);
 }
 
-void expand_stack(struct Stack *stack)
+void
+expand_stack (struct Stack *stack)
 {
 	/**
 	 * stack should only expand as per the required size
 	 * needed to hold current push item
 	 * only handles integers therefore use sizeof(int)
 	 */
-	// there is no need to cast to an int unless it's c++
-	stack->array = realloc(stack->array, sizeof(int) * 10);
-	/* update capacity*/
-	stack->capacity++;
+  /* there is no need to cast to an int unless it's c++*/
+  stack->array = realloc (stack->array, sizeof (int) * 10);
+  /* update capacity */
+  stack->capacity++;
 }
- 
-// Stack is full when top is equal to the last index
-int isFull(struct Stack* stack)
+
+/* Stack is full when top is equal to the last index*/
+int
+isFull (struct Stack *stack)
 {
-    return stack->top == stack->capacity - 1;
+  return (stack->top == (int) stack->capacity - 1);
 }
- 
-// Stack is empty when top is equal to -1
-int isEmpty(struct Stack* stack)
+
+/* Stack is empty when top is equal to -1*/
+int
+isEmpty (struct Stack *stack)
 {
-    return stack->top == -1;
+  return (stack->top == -1);
 }
- 
-// Function to add an item to stack.  It increases top by 1
-void push(struct Stack* stack, int item)
+
+/* Function to add an item to stack.  It increases top by 1*/
+void
+push (struct Stack *stack, int item)
 {
-    if (isFull(stack))
+  if (isFull (stack))
     {
-	    /*if stack is full dynamically expand its size by 1.5 using realloc*/
-	    printf("\narray capacity expanded from %d ",stack->capacity);
-	    expand_stack(stack);
-        printf("to %d capacity\n",stack->capacity);
-	    push(stack, item);
-        //return;
+      /*if stack is full dynamically expand its size by 1.5 using realloc */
+      printf ("\narray capacity expanded from %d ", stack->capacity);
+      expand_stack (stack);
+      printf ("to %d capacity\n", stack->capacity);
+      push (stack, item);
     }
-    else {
-        stack->array[++stack->top] = item;
-        printf("%d pushed to stack\n", item);
+  else
+    {
+      stack->array[++stack->top] = item;
+      printf ("%d pushed to stack\n", item);
     }
 }
- 
-// Function to remove an item from stack.  It decreases top by 1
-int pop(struct Stack* stack)
+
+/* Function to remove an item from stack.  It decreases top by 1*/
+int
+pop (struct Stack *stack)
 {
-    if (isEmpty(stack))
-        return INT_MIN;
-    return stack->array[stack->top--];
-}
- 
-// Function to return the top from stack without removing it
-int peek(struct Stack* stack)
-{
-    if (isEmpty(stack))
-        return INT_MIN;
-    return stack->array[stack->top];
+  if (isEmpty (stack))
+    return (INT_MIN);
+  return (stack->array[stack->top--]);
 }
 
-// Function that returns boolean value
-int is_palindrome(listint_t **head)
+/* Function to return the top from stack without removing it*/
+int
+peek (struct Stack *stack)
+{
+  if (isEmpty (stack))
+    return (INT_MIN);
+  return (stack->array[stack->top]);
+}
+
+/* Function that returns boolean value*/
+int
+is_palindrome (listint_t **head)
 {
 
-        // Temp pointer
-        listint_t *slow = *head;
-        listint_t *walker = *head;
+  listint_t *slow = *head;
+  listint_t *walker = *head;
 
-        // Create a stack
-	struct Stack* stack = createStack(0);
+  /* Create a stack*/
+  struct Stack *stack = createStack (0);
 
 
-        // First traversal to push all the elements to stack
-        while(slow != NULL){
-                push(stack,slow->n);
-                slow = slow->next;
-        }
+  /* First traversal to push all the elements to stack*/
+  while (slow != NULL)
+    {
+      push (stack, slow->n);
+      slow = slow->next;
+    }
 
-        // Second Traversal to compare the stack and node
-        while(walker != NULL ){
-		//printf("val %d == %d\n", walker->n, peek(stack));
+  /* Second Traversal to compare the stack and node*/
+  while (walker != NULL)
+    {
+      /*printf("val %d == %d\n", walker->n, peek(stack));*/
 
-            int i= peek(stack);
-            pop(stack);
+      int i = peek (stack);
+      pop (stack);
 
-            // Compare data
-            if(walker->n != i){
-                return 0;
-            }
-        walker=walker->next;
-        }
+      /* Compare data*/
+      if (walker->n != i)
+	{
+	  return (0);
+	}
+      walker = walker->next;
+    }
 
-return 1;
+  return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -6,21 +6,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-// A structure to represent a stack
-struct Stack {
-    int top;
-    unsigned capacity;
-    int* array;
+
+struct Stack
+{
+  int top;
+  unsigned capacity;
+  int *array;
 };
 
-struct Stack* createStack(unsigned capacity);
-void expand_stack(struct Stack *stack);
-int isFull(struct Stack* stack);
-int isEmpty(struct Stack* stack);
-void push(struct Stack* stack, int item);
-int pop(struct Stack* stack);
-int peek(struct Stack* stack);
+struct Stack *createStack (unsigned capacity);
+void expand_stack (struct Stack *stack);
+int isFull (struct Stack *stack);
+int isEmpty (struct Stack *stack);
+void push (struct Stack *stack, int item);
+int pop (struct Stack *stack);
+int peek (struct Stack *stack);
 
 
 /**
@@ -33,14 +33,14 @@ int peek(struct Stack* stack);
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+  int n;
+  struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+size_t print_listint (const listint_t *h);
+listint_t *add_nodeint_end (listint_t **head, const int n);
+void free_listint (listint_t *head);
 
-int is_palindrome(listint_t **head);
+int is_palindrome (listint_t **head);
 
 #endif /* LISTS_H */
