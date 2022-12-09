a8761df48a2bf3b6a43cebddf55b7f9366bb8120
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-10 01:11:31 +0300

    modified:   0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    #mcopy = [x[:] for x in matrix]
+    if not matrix:
+        return matrix
     mcopy = list()
     for item in matrix:
         mcopy.append(item.copy())
-    #mcopy = matrix.copy()
     for i in range(len(mcopy)):
         for j in range(len(mcopy)):
             mcopy[i][j] *= mcopy[i][j]
