6cf8834172ec1a8c1349b95918fe9eaa0b99c3c8
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-10 01:57:37 +0300

    Revert "	modified:   101-square_matrix_map.py"
    This reverts commit d25829bedc7c40f059ecf46c63ff10e6752eb62f.

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x**2, sublist)) for sublist in matrix]
+    def square(x): return x**2
+    return [list(map(square, sublist)) for sublist in matrix]
