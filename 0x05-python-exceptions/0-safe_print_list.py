33245528f71264b408af0f0feddfd42efd36d282
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-19 21:05:08 +0300

    modified:   0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if !my_list:
-        return (0)
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
