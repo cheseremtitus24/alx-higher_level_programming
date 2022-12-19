a942752ee77c138139d57c1ca8885a8ad16a020b
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-19 21:48:29 +0300

    Fix: Fixed Integer counter

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     temp_val = None
-    for i in range(x):
-        temp_val = my_list[i]
-        if isinstance(temp_val, int):
-            print("{:d}".format(temp_val), end="")
-            i = i + 1
+    counter = 0
+    try:
+        for i in range(x):
+            temp_val = my_list[i]
+            if isinstance(temp_val, int):
+                print("{:d}".format(temp_val), end="")
+                counter = counter + 1
+    except IndexError as e:
+        print("\nIndexError: {}".format(e), end="")
 
-    if i:
-        print()
-    else:
-        i = i - 1
-    return (i)
+    finally:
+
+        if counter:
+            print()
+        return (counter)
