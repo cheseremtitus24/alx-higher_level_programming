c4dbf717e006aa01a9fab021f445b7916feb91f4
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-19 21:07:00 +0300

    modified:   2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,5 +12,8 @@ def safe_print_list_integers(my_list=[], x=0):
     else:
         pass
     finally:
-        print()
+        if i:
+            print()
+        else:
+            i = i - 1
         return (i)
