b6c568844245c9964d4c6e260f91e7d395833f38
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-19 21:50:41 +0300

    modified:   0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        i = i + 1
+            counter = counter + 1
     except BaseException:
         pass
     finally:
-        if i:
+        if counter:
             print()
-        else:
-            i = i - 1
-        return (i)
+        return (counter)
