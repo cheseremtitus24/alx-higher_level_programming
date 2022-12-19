c06dd06e028a9242c2140a21b95ee82f86074fd9
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2022-12-19 21:18:08 +0300

    modified:   2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     temp_val = None
-    try:
-        for i in range(x):
-            temp_val = my_list[i]
-            if isinstance(temp_val, int):
-                print("{:d}".format(temp_val), end="")
-                i = i + 1
-    except BaseException:
-        pass
+    for i in range(x):
+        temp_val = my_list[i]
+        if isinstance(temp_val, int):
+            print("{:d}".format(temp_val), end="")
+            i = i + 1
+
+    if i:
+        print()
     else:
-        pass
-    finally:
-        if i:
-            print()
-        else:
-            i = i - 1
-        return (i)
+        i = i - 1
+    return (i)
