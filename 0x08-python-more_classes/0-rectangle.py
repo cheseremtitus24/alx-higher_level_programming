c07e07269322c1b4f16f9ca02414028b1941dfe7
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-04 23:23:10 +0300

    new file:   0-main.py 	new file:   0-rectangle.py 	new file:   1-main.py 	new file:   1-rectangle.py 	new file:   2-main.py 	new file:   2-rectangle.py 	new file:   3-main.py 	new file:   3-rectangle.py 	new file:   4-main.py 	new file:   4-rectangle.py 	new file:   5-main.py 	new file:   5-rectangle.py 	deleted:    ../0x07-python-test_driven_development/.6-max_integer.py.swp 	deleted:    ../0x07-python-test_driven_development/docs/running_doctests_by_module/byunittestSuite/__pycache__/module.cpython-310.pyc 	../0x05-python-exceptions/__pycache__/ 	../0x06-python-classes/__pycache__/ 	__pycache__/ ../docs/

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+"""
+This module defines an empty rectangle parent class
+"""
+
+
+class Rectangle:
+    """ This is an empty class """
+    pass
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,67 @@
+"""
+This module defines a rectangle parent class
+that has instance methods for data encapsulation using
+the @property decorator function.
+
+"""
+
+
+class Rectangle:
+    """ This is an empty class """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Width getter method returns a private object that is immutable
+        Width setter: """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) not in [int]:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @width.deleter
+    def width(self):
+        pass
+
+    @property
+    def height(self):
+        """Width getter method returns a private object that is immutable
+        Width setter: """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) not in [int]:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @height.deleter
+    def height(self):
+        pass
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (2 * self.width) + (2 * self.height)
+
+    def __str__(self):
+        string = ""
+        for row in range(self.height):
+            for column in range(self.width):
+                string += "#"
+            string += "\n"
+        return string[:-1]
+
+    def __repr__(self):
+        return f'Rectangle({self.width},{self.height})'
