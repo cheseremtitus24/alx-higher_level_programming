e538901e17fdd47172d7e44e37a8b806d88b8707
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-05 10:12:55 +0300

    modified:   0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,15 @@
 """
-This module defines an empty rectangle parent class
+This module defines an empty
+rectangle
+parent class
+that does nothing
+
 """
 
 
 class Rectangle:
-    """ This is an empty class """
+    """ This is an empty class
+    that neither has class attributes
+    nor class instance methods/attributes
+    """
     pass
