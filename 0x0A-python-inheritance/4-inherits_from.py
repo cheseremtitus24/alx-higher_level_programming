99fae3785d3ba0f334c8fbc362a44a0d915c8e3e
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-11 09:41:19 +0300

    Update: Handles case when object is directly an object of the current class by using the (type(obj) not in [probe_class])

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,7 +13,7 @@ def inherits_from(obj, a_class):
     object and checks to verify that the object is an instance
     of the class and returns True else False
     """
-    if (issubclass(type(obj), a_class)):
+    if (issubclass(type(obj), a_class)) and type(obj) not in [a_class]:
         return True
     else:
         return False
