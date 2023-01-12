188ea78eba457bdfd66075f24b3f5aa875fc297d
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-12 20:26:32 +0300

    Docs: Added Module Documentation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import json
 
+"""
+This module contains a function that
+reads a json string and loads it up
+to memory as a python object
+"""
+
 
 def from_json_string(my_obj):
     """
