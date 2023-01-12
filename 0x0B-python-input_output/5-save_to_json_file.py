af93a1446563a635d7b290ea6cfcf36571420b33
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-12 20:37:16 +0300

    modified:   5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,5 @@ def save_to_json_file(my_obj, filename):
     This function writes json dump contents to a file
     and returns the number of characters written to the file
     """
-    with open(filename, mode="w", "utf-8") as f:
+    with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(json.dumps(my_obj))
