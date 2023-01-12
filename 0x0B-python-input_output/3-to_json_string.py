5289864d268956ca56cb0c0f8a923be1353ecd2b
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-12 20:21:17 +0300

    modified:   3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -12,5 +12,5 @@ def to_json_string(my_obj):
     Function serializes a parameter object to json
     and returns its json representation
     """
-    value = json.dumps(my_obj)
+    value = json.dumps(my_obj, sort_keys=True)
     return value
