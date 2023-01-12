8d4440ea8e2dcf5b4bc13ca4fc6f518c58c0872c
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-12 19:41:53 +0300

    modified:   2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,5 +12,5 @@ def append_write(filename="", text=""):
     contents of an input text to a file
     and returns the number of characters written to the file
     """
-    with open(filename, mode="a", "utf-8") as f:
+    with open(filename, mode="a+", "utf-8") as f:
         return f.write(text)
