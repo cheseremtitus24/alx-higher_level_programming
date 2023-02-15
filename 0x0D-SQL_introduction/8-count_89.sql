0667cefba5c91b39fed8cd361b3228f5045fd4e7
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-15 21:41:07 +0300

    fix: Fixed Missing Table Name

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
---counts the number of occurrences of an column id with the value of 89
-SELECT COUNT(*) WHERE id=89;
+-- counts the number of occurrences of an 
+-- column id with the value of 89
+SELECT COUNT(*) FROM first_table WHERE id=89;
