32cc87081d1b4e2d050d2a7aed710babe72d546d
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-15 21:45:56 +0300

    refactor: Switched name and score for Proper Ordering in Output

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Script displays scores greater than 9 and name columns while
 -- ordering by the score in DESCENDING ORDER
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
