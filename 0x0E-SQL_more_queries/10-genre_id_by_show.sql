4363514bf0c10d76e14db0b98dc698cfed37175f
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 14:32:49 +0300

    new file:   10-genre_id_by_show.sql 	../Docs-SQL-Constraints 	../Docs-SQL-User_MNGR 	../SqlJoins.png

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- Shows all tv shows that have been linked
+-- to atleast 1 genre
+SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON  tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
