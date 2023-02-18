7fa0aea48b34c42a5c455cfa5078f0434d16cd77
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 14:54:44 +0300

    modified:   2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,8 @@
 -- Also creates the user
 -- user_0d_2 with password user_0d_2_pwd
 -- Handles Duplicates of DB_name and DB_User
+-- Grants select permissions to user
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Granting only Select global permission
 GRANT SELECT on 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 
