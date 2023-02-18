8df4020ef98658801ff9e279325bc9e145d2a702
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 14:58:22 +0300

    modified:   4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 -- id_not_null that has 2 fields
 -- id and name
 -- the id field has a default value of 1
-CREATE TABLE IF NOT EXISTS 'id_not_null'(id INT DEFAULT=1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT=1, name VARCHAR(256));
