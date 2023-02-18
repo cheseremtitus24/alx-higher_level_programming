a8466c85ce96899fd8d67d2a8d7f43d970ed4199
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 14:47:33 +0300

    modified:   1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- user_0d_1 
 -- Handles error when user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
