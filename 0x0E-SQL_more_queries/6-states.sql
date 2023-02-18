21b3b0f5b2a7c40659d163a3b4f67772891d35cf
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 15:07:23 +0300

    modified:   6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script creates the database
--- hbtn_0d_2
+-- hbtn_0d_usa
 -- Also creates the user
 -- user_0d_2 with password user_0d_2_pwd
 -- Handles Duplicates of DB_name and DB_User
@@ -10,5 +10,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- the id field has a default value of 1
 -- id filed is constrained as being UNIQUE
 -- is a primary key and auto_increments
-CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL  PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE NOT NULL  PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
 
