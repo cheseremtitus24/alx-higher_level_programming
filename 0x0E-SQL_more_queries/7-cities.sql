09e7eb0adf250ae2e0e084b5a98b0567f081984a
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 15:14:17 +0300

    modified:   7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@
 -- Also creates the user
 -- user_0d_2 with password user_0d_2_pwd
 -- Handles Duplicates of DB_name and DB_User
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- script creates a database TABLE
 -- cities that has 3 fields
@@ -12,4 +12,4 @@ CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 -- id filed is constrained as being UNIQUE
 -- is a primary key and auto_increments
 -- Makes use of FOREIGN key CONSTRAINT
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa.cities'(id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL); 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL); 
