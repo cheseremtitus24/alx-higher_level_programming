e2fb914d362219bb35be5340cfda1ab7b1772daf
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-02-18 14:56:50 +0300

    modified:   3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
 -- script creates a database TABLE
--- force_name that has 2 fields
--- id and name
--- the name field must not be null
-CREATE TABLE IF NOT EXISTS 'force_name'(id INT, name VARCHAR(256) NOT NULL);
+-- force_name that has 2 fields id and name the name field must not be null
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
 
