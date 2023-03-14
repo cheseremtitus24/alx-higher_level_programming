244c2f640ba28673e12043ed08572b3ebebd8e27
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-14 13:28:07 +0300

    fix: Handle Case when argument is zero: should now print number 0 in output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const valToInt = parseInt(process.argv[2]);
-(valToInt)
+
+(valToInt || valToInt === 0)
   ? console.log('My number : ' + valToInt)
   : console.log('Not a number');
