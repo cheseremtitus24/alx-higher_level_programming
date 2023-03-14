9f50f6c5f7e9c49c81aab834cfa02d7597f34cbb
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-14 13:05:46 +0300

    feature: Handle Weak Type Ref Conversion
    Instead of converting an integer to a string for concatenation the function add(a,b) return NaN instead

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,13 +2,12 @@
 /*
  * This script sums commandline
  * arguments 1 and 2 and prints out the results
- * Make use of an anonymous function
- * that makes use of an arrow function
  */
 const valToInt = parseInt(process.argv[2]);
 const val2ToInt = parseInt(process.argv[3]);
-(valToInt && val2ToInt)
-  ? (() => {
-      console.log(valToInt + val2ToInt);
-    })()
-  : console.log(NaN);
+
+if (!isNaN(valToInt) && !isNaN(val2ToInt)) {
+  console.log(valToInt + val2ToInt);
+} else {
+  console.log(NaN);
+}
