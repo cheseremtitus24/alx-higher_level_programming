eb8128ae201ddefe8f27c2b2545f60f89fd161ea
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-14 13:41:01 +0300

    Refactoring: Used if..else.. in Place of Ternary Operator

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const valToInt = parseInt(process.argv[2]);
 
-(valToInt || valToInt === 0)
-  ? console.log('My number : ' + valToInt)
-  : console.log('Not a number');
+if (valToInt || valToInt === 0) {
+  console.log('My number : ' + valToInt)
+} else {
+  console.log('Not a number');
+}
