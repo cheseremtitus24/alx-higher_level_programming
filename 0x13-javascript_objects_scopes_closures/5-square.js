0523f1d58b4ad841940ac76efaf20c9417e06ad2
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-03-16 14:08:11 +0300

    feat: Class Inheritance in Javascript

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((parseInt(w) > 0) && (parseInt(h) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let out = '';
+      for (let j = 0; j < this.width; j++) {
+        out += 'X';
+      }
+      console.log(out);
+    }
+  }
+
+  rotate () {
+    // Swaps width and height
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    // Doubles the width and height
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
