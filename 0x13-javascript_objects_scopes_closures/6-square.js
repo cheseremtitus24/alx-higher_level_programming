dce974cde22f1b860170357f12038cf233e750e7
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-03-16 14:28:59 +0300

    Refactored: Added a Module Import Expression for a Square Class Defined in '5-square.js' File

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,41 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((parseInt(w) > 0) && (parseInt(h) > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square = require('./5-square.js');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let out = '';
-      for (let j = 0; j < this.width; j++) {
-        out += 'X';
-      }
-      console.log(out);
-    }
-  }
-
-  rotate () {
-    // Swaps width and height
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    // Doubles the width and height
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
 module.exports = class PSquare extends Square {
   charPrint (symbol = 'X') {
     for (let i = 0; i < this.height; i++) {
