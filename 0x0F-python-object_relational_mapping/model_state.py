3eb6a73fb38c6630b86f6afa8a49816663f2fbb3
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-19 22:08:28 +0300

    docs: Added Module and Class Documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,3 +1,7 @@
+"""
+This module creates a table named states
+that has an id and a name field
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -5,6 +9,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    This is a class that creates
+    the schema of a table named
+    states that will contain two columns
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
