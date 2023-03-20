0f5adac0226079f060d4ab801e2043f00af8557d
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-20 19:28:39 +0300

    new file:   14-model_city_fetch_by_state.py 	new file:   14-model_city_fetch_by_state.sql 	new file:   model_city.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module Makes use of an ORM
+that Retrieves rows from the table states
+and orders them in ascending order based
+on the primary key column
+"""
+import sys
+
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+from model_city import Base, City
+
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    query = session.query(
+        City,
+        State).join(
+        State,
+        City.state_id == State.id).order_by(
+            City.id)
+    for _row in query.all():
+        print(_row[1].name + ": (" + str(_row[0].id) + ") " + _row[0].name)
+        # print("{:d}: {:s}".format(_row.id, _row.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module creates a table named states
+that has an id and a name field
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    This is a class that creates
+    the schema of a table named
+    states that will contain two columns
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
