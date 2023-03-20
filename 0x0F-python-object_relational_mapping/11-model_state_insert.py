0e20603182597c576481fc3b91bc42143410bd01
Cheserem Titus <cheseremtitus24@gmail.com>
2023-03-20 16:07:18 +0300

    new file:   11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module Makes use of an ORM
+that Retrieves the first row from the table states
+and orders them in ascending order based
+on the primary key column
+if table is empty it return prints 'Nothing'
+"""
+import sys
+
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    users = [
+        State(name='Louisiana'
+              ),
+    ]
+
+    # add row entries all
+    session.add_all(users)
+    session.commit()
