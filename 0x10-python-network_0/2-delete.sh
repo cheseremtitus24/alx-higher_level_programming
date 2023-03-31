7affeab41b0f1af58608a982491dc37ff9c20699
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-03-31 11:00:56 +0300

    modified:   2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Sends a DELETE HTTP verb to Delete a resource
+if [ -z "$1" ]; then
+  echo "Please provide a URL as an argument."
+  exit 1
+fi
 commandline_argument_1=$1
 curl -X DELETE -s "$commandline_argument_1" -H "Connection: close"
