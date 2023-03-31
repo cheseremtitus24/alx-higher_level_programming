6df92c018e6749809f83bf003f090144f63c0832
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-03-31 09:10:59 +0300

    modified:   1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script returns the contents of body response if a Status Code of 200 is reported
 commandline_argument_1=$1
-status_code=$(curl -sLI "$commandline_argument_1" | grep 200|  cut -d " " -f 2)
-page_body=$(curl -sL "$commandline_argument_1")
+status_code=$(curl -X GET -sLI "$commandline_argument_1" | grep 200|  cut -d " " -f 2)
+page_body=$(curl -X GET -sL "$commandline_argument_1")
 
 if test "$status_code" = "200"
 then
