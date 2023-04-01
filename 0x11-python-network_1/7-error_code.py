6203cd976cfb8d5f706d6afcf2a8e7614d128311
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-01 19:16:26 +0300

    new file:   7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module makes use of the requests module to send a get request to a
+web url then output its payload on-screen
+"""
+import sys
+
+import requests
+from requests import HTTPError
+
+
+def make_request(url, data=None, method='get'):
+    """
+        :param url: resource URL to get content from
+        :return: request body and http client object
+    """
+    try:
+        with requests.Session() as s:
+            if method == 'get':
+                response = s.get(url=url, data=data, timeout=10)
+            elif method == 'post':
+                response = s.post(url=url, data=data, timeout=10)
+
+        # print(response.status_code)
+        return response.text, response
+    except HTTPError as error:
+        print(error.status, error.reason)
+    except TimeoutError:
+        print("Request timed out")
+
+
+if __name__ == '__main__':
+    args_length = len(sys.argv)
+    if args_length == 2:
+        url = sys.argv[1]
+        # body, response = make_request("https://alx-intranet.hbtn.io/status")
+        try:
+            body, response = make_request(url=url, method='get')
+            # if status_code is btwn 200 and 400 response == True Else False
+            if response:
+                print(body)
+            else:
+                print("Error code:", response.status_code)
+
+        except BaseException as e:
+            print(e)
