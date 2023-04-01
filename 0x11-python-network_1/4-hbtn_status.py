54b76de336b780e694e5a3fc7fcc1644fb695c4a
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-01 17:38:39 +0300

    Feat: Implemented Use of a Context Manager with Requests Module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,9 @@ web url then output its payload on-screen
 """
 
 import requests
+from contextlib import closing
+
+from requests import HTTPError
 
 
 def make_request(url):
@@ -12,11 +15,20 @@ def make_request(url):
         :param url: resource URL to get content from
         :return: request body and http client object
     """
-    response = requests.get(url)
-    return (response.text, response)
+    try:
+        with closing(requests.get(url=url, stream=True, timeout=10)) as response:
+            # print(response.status)
+            return (response.text, response)
+    except HTTPError as error:
+        print(error.status, error.reason)
+    except TimeoutError:
+        print("Request timed out")
 
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    body, response = make_request(url=url)
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(body), body))
+    try:
+        body, response = make_request(url=url)
+        print("Body response:\n\t- type: {}\n\t- content: {}".format(type(body), body))
+    except BaseException as e:
+        pass
