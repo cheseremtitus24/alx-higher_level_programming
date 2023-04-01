5be839f20f69858bbf54d0cca52578db485462e2
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-04-01 16:58:26 +0300

    Refactored: Removed Unused Varibles

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,17 +7,12 @@ web url then output its payload on-screen
 import requests
 
 
-def make_request(url, params=None, data=None, headers=None):
+def make_request(url):
     """
         :param url: resource URL to get content from
-        :param headers: Custom Headers to be passed in e.g. Custom User-Agent
-        :param params: post data to be sent with a POST HTTP Verb
         :return: request body and http client object
-        :raises: No exceptions but handles HTTPError,
-        URLError and TimeoutError
     """
     response = requests.get(url)
-    # print(response.status_code)
     return (response.text, response)
 
 
